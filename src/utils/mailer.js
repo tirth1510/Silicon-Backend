aad11323c85db28b2b882eabcfe97dc1a5e2b7e2
tirth1510 <tirthadiyecha103@gmail.com>
2026-02-21 16:18:48 +0530

    update email tamplate 4

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -4,14 +4,11 @@ export const sendEmail = async ({ to, subject, html }) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
-      port: 465,
+      port: Number(process.env.SMTP_PORT),
       secure: true, // ALWAYS TRUE for Gmail + 465
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false,
       },
     });
 
